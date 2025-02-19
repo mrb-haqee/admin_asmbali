9493d55b8f3e458ae7a469dadb0500d81ad99723
mrb-haqee <raflibahaqi890@gmail.com>
2025-02-19 16:00:01 +0800

    feat: add Livewire components and views for role management

---
+++ app/Livewire/Konfigurasi/Aksesibilitas/Roles/DataRoles.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Konfigurasi\Aksesibilitas\Roles;
+
+use Livewire\Component;
+
+class DataRoles extends Component
+{
+    public function render()
+    {
+        return view('livewire.konfigurasi.aksesibilitas.roles.data-roles');
+    }
+}
---
+++ app/Livewire/Konfigurasi/Aksesibilitas/Roles/FormRoles.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Konfigurasi\Aksesibilitas\Roles;
+
+use Livewire\Component;
+
+class FormRoles extends Component
+{
+    public function render()
+    {
+        return view('livewire.konfigurasi.aksesibilitas.roles.form-roles');
+    }
+}
