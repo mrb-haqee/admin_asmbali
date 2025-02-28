40a47144970d2bfc685effc7e077ec23945cc66a
mrb-haqee <raflibahaqi890@gmail.com>
2025-02-28 17:11:04 +0800

    feat: add Modal Container component with customizable size property

---
+++ app/View/Components/Modal/Container.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\View\Components\Modal;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class Container extends Component
+{
+    public $id;
+    public $sizeModal;
+
+    /**
+     * Create a new component instance.
+     *
+     * @param string $id Modal
+     * @param string $title
+     */
+    public function __construct(string $id = 'default', string $sizeModal = '')
+    {
+        $this->id = $id;
+        $this->sizeModal = $sizeModal;
+    }
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.modal.container', [
+            'id' => $this->id,
+            'sizeModal' => $this->sizeModal,
+        ]);
+    }
+}
