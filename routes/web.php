93acf3d2f7ce6f6d8409d20207746c8c1aaa9281
mrb-haqee <mrb.haqee@gmail.com>
2025-03-02 16:02:06 +0800

    feat: add account route to masterdata in web.php

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\DashboardController;
 use App\Livewire\Konfigurasi\Aksesibilitas\Permission\PermissionIndex;
 use App\Livewire\Konfigurasi\Aksesibilitas\Roles\Detail\RolesSubIndex;
 use App\Livewire\Konfigurasi\Aksesibilitas\Roles\RolesIndex;
+use App\Livewire\Konfigurasi\Masterdata\Account\AccountIndex;
 use App\Livewire\Konfigurasi\Masterdata\Menu\Detail\MenuSubIndex;
 use App\Livewire\Konfigurasi\Masterdata\Menu\MenuIndex;
 use App\Livewire\Konfigurasi\Masterdata\User\UserIndex;
@@ -36,8 +37,10 @@ Route::middleware(['auth', 'verified', 'menu.access'])->group(function () {
         Route::name('masterdata.')->prefix('masterdata')->group(function () {
             Route::get('menu', MenuIndex::class)->name('menu.index');
             Route::get('menu/{menu}', MenuSubIndex::class)->name('menu.show');
-
+            
             Route::get('user', UserIndex::class)->name('user.index');
+
+            Route::get('account', AccountIndex::class)->name('account.index');
         });
 
         Route::name('aksesibilitas.')->prefix('aksesibilitas')->group(function () {
@@ -47,46 +50,6 @@ Route::middleware(['auth', 'verified', 'menu.access'])->group(function () {
             Route::get('permission', PermissionIndex::class)->name('permission.index');
         });
     });
-    // Route::name('konfigurasi.')->prefix('konfigurasi')->group(function () {
-    //     Route::name('masterdata.')->prefix('masterdata')->group(function () {
-    //         Route::get('menu', function () {
-    //             return view('livewire.konfigurasi.masterdata.menu.index');
-    //         })->name('menu');
-    //         Route::get('menu/{id}', function ($id) {
-    //             $menu = Menu::find($id)->toArray();
-    //             return view('livewire.konfigurasi.masterdata.menu.detail.index', ['menu_id' => $id, 'menu' => $menu]);
-    //         })->name('menu.show');
-    //     });
-    // });
-
-
-    // routes/web.php
-
-    // Route::name('account.')->prefix('account')->group(function () {
-    //     Route::resource('data', MenuController::class)->names('data');
-    // });
-
-    // Route::name('user.')->prefix('user')->group(function () {
-    //     Route::resource('data', MenuController::class)->names('data');
-    // });
-
-    // Route::name('administrasi.')->prefix('administrasi')->group(function () {
-    //     Route::name('laporan.')->prefix('laporan')->group(function () {
-    //         Route::resource('laporan_pemasukan', UserManagementController::class)->names('laporan_pemasukan');
-    //     });
-    // });
-
-    // Route::name('web_asm.')->prefix('web_asm')->group(function () {
-    //     Route::name('content.')->prefix('content')->group(function () {
-    //         Route::resource('landing_page', UserManagementController::class)->names('landing_page');
-    //     });
-    // });
-
-    // Route::name('web_tpq.')->prefix('web_tpq')->group(function () {
-    //     Route::name('content.')->prefix('content')->group(function () {
-    //         Route::resource('landing_page', UserManagementController::class)->names('landing_page');
-    //     });
-    // });
 });
 
 
